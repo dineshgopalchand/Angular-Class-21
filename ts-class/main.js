8180fb1d4d422990bb4cc869a8112c54e021e335
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2021-04-16 09:05:22 +0530

    ts introduction

---
+++ ts-class/main.js
@@ -0,0 +1,7 @@
+function showName(name) {
+    return 'some name ' + name;
+    // console.log('some name ' + name);
+}
+var nameVal = 'SDLC Training';
+var result = showName(nameVal);
+console.log(result.toLowerCase());
