2ef65759122803eaeb608964b4898349039376a8
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2021-04-16 09:05:46 +0530

    variable intro

---
+++ ts-class/variable.js
@@ -0,0 +1,23 @@
+var anyVal;
+anyVal = 1;
+anyVal = true;
+anyVal = {};
+var anyVal1 = 1;
+// let schoolName;
+// schoolName = 'SDLC';
+// schoolName = 1;
+var schoolName;
+schoolName = 'SDLC';
+schoolName = 'SDLC Training';
+// schoolName = 1; // it will show error
+var rollNumber;
+rollNumber = 1;
+rollNumber.toString();
+var bool;
+bool = true;
+var numArr;
+numArr = [1, 2, 3, 4, 4, 4, 4];
+var numArr1 = [1, 2, 3, 4, 4, 4, 4, [2, 3, 3]];
+var numArr2 = [[1, 3], [233], [2, 3, 3]];
+// let numArr3: (number | number[] | (number | string)[])[] = [1, 2, 3, 4, 4, 4, 4, [2, 3, 3], [234324, '2w34']];
+var numStringArr = [1, 3, 3, 4, 4, 'D', 'sd'];
