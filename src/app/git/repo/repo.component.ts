7a1fc3cc9a357724322f36edf7059f1082bc059f
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2021-06-23 10:18:31 +0530

    discussed combination function of rxjs to get value from prams and queryParams at once

--- src/app/git/repo/repo.component.ts
+++ src/app/git/repo/repo.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { JSONFormat } from 'src/app/shared/_help/jsonformat';
+import { OptionDetails } from 'src/app/shared/_helper/data-service';
 import { GitDetailsService } from '../services/git-details.service';
 
 @Component({
@@ -37,10 +39,34 @@ export class RepoComponent implements OnInit {
         console.log(data);
         this.repoList = data as JSONFormat[];
       });
-    this.route.queryParamMap.subscribe(qParams => {
-      this.p = qParams.get('p') || this.p;
-      this.noPerPage = qParams.get('perpage') || this.noPerPage;
-    });
+    this.route.queryParamMap
+      .subscribe(qParams => {
+        this.p = qParams.get('p') || this.p;
+        this.noPerPage = qParams.get('perpage') || this.noPerPage;
+      });
+
+    combineLatest([this.route.params, this.route.parent?.params, this.route.queryParams])
+      .pipe(switchMap(paramsVal => {
+        console.log(paramsVal);
+        const params = paramsVal as any[];
+        this.userName = params[1].username as string;
+        const option: OptionDetails = {};
+        // const qParams = params[2];
+        // const queryParams = {} as JSONFormat;
+        // queryParams.page = qParams.p;
+        // queryParams.per_page = qParams.perpage;
+        // option.params = queryParams;
+        if (this.userName) {
+          console.log(option);
+          return this.gitService.getRepo(this.userName, option);
+        } else {
+          return of([]);
+        }
+
+      })).subscribe(data => {
+        console.log(data);
+        // this.repoList = data as JSONFormat[];
+      });
   }
   pageChanged(e: any): void {
     this.router.navigate([], {
